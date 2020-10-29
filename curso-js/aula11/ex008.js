6248d4e39e639405d4f99213d31a7649a8ca7ba8
André Luiz Freygang <72108085+aFreygang@users.noreply.github.com>
2020-10-29 17:20:28 -0300

    Add files via upload

---
+++ curso-js/aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 3
+console.log(`A velocidade do carro é ${vel}KM/h`)
+
+console.log(`Limite da rodovia: 40KM/h`)
+
+if (vel > 40) { // Condição Simples
+    console.log(`Você ultrapassou o limite máximo. MULTADO!`)
+}
---
+++ curso-js/aula11/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'EUA'
+if (pais == 'Brasil') {
+    console.log('Brazilian')
+} else {
+    console.log('American')
+}
